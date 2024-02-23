b2ae2d1ba23e5952b29bc63e7eb6de1c7cb83086
Andrew Lind <andrewgrantlind@gmail.com>
2024-02-23 21:12:15 +1300

    Add Neuroreceptor-Dependant Plasticity (NRDP) learning rule

--- neucube/reservoir.py
+++ neucube/reservoir.py
@@ -1,8 +1,36 @@
 import torch
 from tqdm import tqdm
 import math
+from numpy.random import uniform as unif
 from .topology import small_world_connectivity
 from .utils import print_summary
+
+# NRDP constants
+# fine-tuning is necessary for better classification results
+MIN_A = 0.0
+MAX_A = 0.901679611594126
+GAIN_A = 0.5428717518563672
+MIN_N = 0.0
+MAX_N = 0.23001290732040292
+GAIN_N = 0.011660312977761912
+MIN_GA = 0.0
+MAX_GA = 0.7554145024515596
+GAIN_GA = 0.3859076787035615
+MIN_GB = 0.0
+MAX_GB = 0.7954714253083993
+GAIN_GB = 0.11032115434326673
+
+def is_gaba_activated(gaba_rate=0.7):
+  return unif(0, 1) < gaba_rate
+
+def get_gaba_gain():
+  return GAIN_GA if is_gaba_activated() else GAIN_GB
+
+def get_gaba_min():
+  return MIN_GA if is_gaba_activated() else MIN_GB
+
+def get_gaba_max():
+  return MAX_GA if is_gaba_activated() else MAX_GB
 
 class Reservoir():
   def __init__(self, cube_shape=(10,10,10), inputs=None, coordinates=None, mapping=None, c=1.2, l=1.6, c_in = 0.9, l_in = 1.2):
@@ -22,6 +50,8 @@ class Reservoir():
         l_in (float): Parameter controlling the connectivity of the input neurons.
     """
     self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu:0")
+    # use Metal Performance Shaders (beta)
+    #self.device = torch.device("mps:0" if torch.backends.mps.is_available() else "cpu:0")
 
     if coordinates is None:
       self.n_neurons = math.prod(cube_shape)
@@ -63,6 +93,7 @@ class Reservoir():
 
     spike_rec = torch.zeros(self.batch_size, self.n_time, self.n_neurons)
 
+    # iterate over each sample
     for s in tqdm(range(X.shape[0]), disable = not verbose):
 
       spike_latent = torch.zeros(self.n_neurons).to(self.device)
@@ -71,30 +102,73 @@ class Reservoir():
       refrac_count = torch.zeros(self.n_neurons).to(self.device)
       spike_times = torch.zeros(self.n_neurons).to(self.device)
 
+      # NRDP specific
+      firing_state = torch.zeros(self.n_neurons).to(self.device)
+      a_state = torch.zeros(self.n_neurons).to(self.device)
+      a_t = torch.zeros(self.n_neurons).to(self.device)
+      n_t = torch.zeros(self.n_neurons).to(self.device)
+      g_t = torch.zeros(self.n_neurons).to(self.device)
+      time_window = 10
+      gaba_impact = 0.01
+
+      # iterate over each row of data (time steps)
       for k in range(self.n_time):
 
-        spike_in = X[s,k,:]
+        spike_in = X[s,k,:] # one row of data i.e. a time step
         spike_in = spike_in.to(self.device)
 
         refrac[refrac_count < 1] = 1
 
-        I = torch.sum(self.w_in*spike_in, axis=1)+torch.sum(self.w_latent*spike_latent, axis=1)
-        mem_poten = mem_poten*torch.exp(torch.tensor(-(1/40)))*(1-spike_latent)+(refrac*I)
+        # calculate membrane potential
+        I = torch.sum(self.w_in*spike_in, axis=1)+torch.sum(self.w_latent*spike_latent, axis=1) # current
+        mem_poten = mem_poten*torch.exp(torch.tensor(-(1/40)))*(1-spike_latent)+(refrac*I) # LIF
 
-        spike_latent[mem_poten >= mem_thr] = 1
-        spike_latent[mem_poten < mem_thr] = 0
+        # output spike
+        spike_latent[mem_poten >= mem_thr] = 1 # spike
+        spike_latent[mem_poten < mem_thr] = 0 # no spike
 
-        refrac[mem_poten >= mem_thr] = 0
+        refrac[mem_poten >= mem_thr] = 0 # reset after spike emitted
         refrac_count[mem_poten >= mem_thr] = refractory_period
         refrac_count = refrac_count-1
 
-        if train == True:
-          t_constant = 3
+        if train is True:
           self.aux = k-spike_times
-          self.pre_w = 0.0001*torch.exp(-self.aux/t_constant)*torch.gt(self.aux,0).int()
-          self.pos_w = -0.01*torch.exp(-self.aux/t_constant)*torch.gt(self.aux,0).int()
-          pre_updates = self.pre_w*torch.gt((self.w_latent.T*spike_latent).T, 0).int()
-          pos_updates = self.pos_w*torch.gt(self.w_latent*spike_latent, 0).int()
+
+          #region STDP
+          #t_constant = 3
+          #self.pre_w = 0.0001*torch.exp(-self.aux/t_constant)*torch.gt(self.aux,0).int() # ltp
+          #self.pos_w = -0.01*torch.exp(-self.aux/t_constant)*torch.gt(self.aux,0).int() # ltd
+          #pre_updates = self.pre_w*torch.gt((self.w_latent.T*spike_latent).T, 0).int()
+          #pos_updates = self.pos_w*torch.gt(self.w_latent*spike_latent, 0).int()
+          #end region
+
+          # region NRDP
+          # NRDP implementation as per: https://ieeexplore.ieee.org/document/8118188
+          a_t = torch.where(firing_state >= time_window,
+                            torch.maximum(torch.full_like(a_t, MIN_A), a_t-(gaba_impact * get_gaba_gain())),
+                            torch.minimum(torch.full_like(a_t, MAX_A), a_t+GAIN_A))
+          firing_state[spike_latent < 1] += 1
+          # TODO: set A(t) to MAX_A for duration of time window
+          a_t = torch.where(a_t >= MAX_A,
+                            torch.where(a_state < time_window, MAX_A, a_t), a_t)
+          a_state[a_t >= MAX_A] += 1
+          # only calculate N(t) when A(t) reaches its max level and there is a spike
+          n_t = torch.where(a_t >= MAX_A,
+                            torch.where(spike_latent > 0,
+                                        torch.maximum(torch.full_like(n_t, MIN_N), n_t-GAIN_N),
+                                        torch.minimum(torch.full_like(n_t, MAX_N), n_t+GAIN_N)),
+                            MIN_N)
+          g_t = torch.where(spike_latent > 0,
+                            torch.maximum(torch.full_like(g_t, get_gaba_min()), g_t-get_gaba_gain()),
+                            torch.minimum(torch.full_like(g_t, get_gaba_max()), g_t+get_gaba_gain()*self.aux))
+
+          # reset
+          firing_state[firing_state >= time_window] = 0
+          a_state[a_state >= time_window] = 0
+
+          pre_updates = torch.zeros(self.n_neurons, self.n_neurons)
+          pos_updates = (n_t + a_t) - g_t # Wij
+          # endregion
 
           self.w_latent += pre_updates
           self.w_latent += pos_updates
