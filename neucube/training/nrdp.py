2e409d25a10efd718ffa957ab47db8940129c439
Andrew Lind <andrewgrantlind@gmail.com>
2024-02-25 14:39:18 +1300

    refactoring, and update doctrings

--- neucube/training/nrdp.py
+++ neucube/training/nrdp.py
@@ -1,13 +1,33 @@
 import torch
 from numpy.random import uniform as unif
 
-# NRDP implementation as per: https://ieeexplore.ieee.org/document/8118188
 class NRDP():
     def __init__(self, min_a=0.0, max_a=0.901679611594126, gain_a=0.5428717518563672,
                  min_n=0.0, max_n=0.23001290732040292, gain_n=0.011660312977761912,
                  min_ga=0.0, max_ga=0.7554145024515596, gain_ga=0.3859076787035615,
                  min_gb=0.0, max_gb=0.7954714253083993, gain_gb=0.11032115434326673,
                  time_window=10, gaba_impact=0.01, gaba_rate=0.7):
+        """
+        Initializes the NRDP object. This implementation is highly sensitive to values
+        supplied for minimum/maximum Neuroreceptor levels and gaining rates.
+
+        Parameters:
+            min_a (float): Minimum level for AMPAR
+            max_a (float): Maximum level for AMPAR
+            gain_a (float): Gaining rate for AMPAR
+            min_n (float): Minimum level for NMDAR
+            max_n (float): Maximum level for NMDAR
+            gain_n (float): Gaining rate for NMDAR
+            min_ga (float): Minimum level for GABAa
+            max_ga (float): Maximum level for GABAa
+            gain_ga (float): Gaining rate for GABAa
+            min_gb (float): Minimum level for GABAb
+            max_gb (float): Maximum level for GABAb
+            gain_gb (float): Gaining rate for GABAb
+            time_window (int): Time window for AMPAR
+            gaba_impact (float): Rate that the GABA receptors impact AMPAR
+            gaba_rate (float): GABAa activation probability
+        """
         self.min_a = min_a
         self.max_a = max_a
         self.gain_a = gain_a
@@ -37,41 +57,84 @@ class NRDP():
         return self.max_ga if self._is_gaba_activated() else self.max_gb
 
     def setup(self, device, n_neurons):
+        """
+        Hook for performing setup tasks.
+
+        Parameters:
+            device (torch.device): The torch device.
+            n_neurons (int): Number of neurons in the cube.
+        """
         self.device = device
         self.n_neurons = n_neurons
 
     def per_sample(self, s):
-        self.firing_state = torch.zeros(self.n_neurons).to(self.device)
-        self.a_state = torch.zeros(self.n_neurons).to(self.device)
-        self.a_t = torch.zeros(self.n_neurons).to(self.device)
-        self.n_t = torch.zeros(self.n_neurons).to(self.device)
-        self.g_t = torch.zeros(self.n_neurons).to(self.device)
+        """
+        Hook for performing per sample tasks.
+
+        Parameters:
+            s (int): The sample number.
+        """
+        self.firing_state = torch.zeros(self.n_neurons).to(self.device) # Element-wise count since last spike
+        self.a_state = torch.zeros(self.n_neurons).to(self.device) # Element-wise AMPAR state
+        self.a_t1 = torch.zeros(self.n_neurons).to(self.device) # A(t-1)
+        self.n_t1 = torch.zeros(self.n_neurons).to(self.device) # N(t-1)
+        self.g_t1 = torch.zeros(self.n_neurons).to(self.device) # G(t-1)
 
     def per_time_slice(self, s, k):
+        """
+        Hook for performing per time-slice tasks.
+
+        Parameters:
+            s (int): The sample number.
+            k (int): The time-slice number.
+        """
         pass
 
     def train(self, aux, _, spike_latent):
-        a_t = torch.where(self.firing_state >= self.time_window,
-                          torch.maximum(torch.full_like(self.a_t, self.min_a), self.a_t-(self.gaba_impact*self._get_gaba_gain())),
-                          torch.minimum(torch.full_like(self.a_t, self.max_a), self.a_t+self.gain_a))
+        """
+        Implements the NRDP learning rule as per: https://ieeexplore.ieee.org/document/8118188.
+
+        Parameters:
+            aux (torch.Tensor): Element-wise time since last spike.
+            _ (torch.Tensor): Ignored parameter.
+            spike_latent (torch.Tensor): Output spikes.
+        Returns:
+            pre_updates (torch.Tensor): Pre-synaptic updates.
+            pos_updates (torch.Tensor): Post-synaptic updates.
+        """
+        # calculate A(t) when Sij = 1
+        a_t = torch.where(spike_latent > 0,
+                          torch.minimum(torch.full_like(self.a_t1, self.max_a), self.a_t1+self.gain_a),
+                          # calculate A(t) when Sij = 0 for the length of time window
+                          torch.where(self.firing_state >= self.time_window,
+                                      torch.maximum(torch.full_like(self.a_t1, self.min_a), self.a_t1-(self.gaba_impact*self._get_gaba_gain())),
+                                      self.a_t1))
         self.firing_state[spike_latent < 1] += 1
-        # TODO: set A(t) to MAX_A for duration of time window
+        # hold A(t) at MAX_A for duration of time window
         a_t = torch.where(a_t >= self.max_a,
-                          torch.where(self.a_state < self.time_window, self.max_a, a_t),
-                          a_t)
+                          torch.where(self.a_state < self.time_window, self.max_a, self.a_t1),
+                          self.a_t1)
         self.a_state[a_t >= self.max_a] += 1
-        # only calculate N(t) when A(t) reaches its max level and there is a spike
+        # calculate N(t) when A(t) reaches MAX_A and Sij = 1
         n_t = torch.where(a_t >= self.max_a,
                           torch.where(spike_latent > 0,
-                                      torch.maximum(torch.full_like(self.n_t, self.min_n), self.n_t-self.gain_n),
-                                      torch.minimum(torch.full_like(self.n_t, self.max_n), self.n_t+self.gain_n)),
-                          self.min_n)
+                                      torch.minimum(torch.full_like(self.n_t1, self.max_n), self.n_t1+self.gain_n),
+                                      torch.maximum(torch.full_like(self.n_t1, self.min_n), self.n_t1-self.gain_n)),
+                          self.min_n) # N(t) is not activated unless A(t) >= MAX_A
+        # calculate G(t) when Sij = 1
         g_t = torch.where(spike_latent > 0,
-                          torch.maximum(torch.full_like(self.g_t, self._get_gaba_min()), self.g_t-self._get_gaba_gain()),
-                          torch.minimum(torch.full_like(self.g_t, self._get_gaba_max()), self.g_t+self._get_gaba_gain()*aux))
+                          torch.maximum(torch.full_like(self.g_t1, self._get_gaba_min()), self.g_t1-self._get_gaba_gain()),
+                          torch.minimum(torch.full_like(self.g_t1, self._get_gaba_max()), self.g_t1+self._get_gaba_gain()*aux))
+
+        self.a_t1 = a_t
+        self.n_t1 = n_t
+        self.g_t1 = g_t
 
         return torch.zeros(self.n_neurons, self.n_neurons), (n_t + a_t) - g_t  # Wij
 
     def reset(self):
+        """
+        Hook for performing reset tasks.
+        """
         self.firing_state[self.firing_state >= self.time_window] = 0
         self.a_state[self.a_state >= self.time_window] = 0
--- neucube/training/stdp.py
+++ neucube/training/stdp.py
@@ -1,27 +1,69 @@
 import torch
 
 class STDP():
-    def __init__(self, tau_pos=0.0001, tau_neg=-0.01, t_constant=3):
-        self.tau_pos = tau_pos
-        self.tau_neg = tau_neg
+    def __init__(self, a_pos=0.0001, a_neg=-0.01, t_constant=3):
+        """
+        Initializes the STDP object.
+
+        Parameters:
+            a_pos (float): Positive synaptic adjustment.
+            a_neg (float): Negative synaptic adjustment.
+            t_constant (int): Pre- and post-synaptic time interval.
+        """
+        self.a_pos = a_pos
+        self.a_neg = a_neg
         self.t_constant = t_constant
 
     def setup(self, device, n_neurons):
+        """
+        Hook for performing setup tasks.
+
+        Parameters:
+            device (torch.device): The torch device.
+            n_neurons (int): Number of neurons in the cube.
+        """
         self.device = device
         self.neurons = n_neurons
 
     def per_sample(self, s):
+        """
+        Hook for performing per sample tasks.
+
+        Parameters:
+            s (int): The sample number.
+        """
         pass
 
     def per_time_slice(self, s, k):
+        """
+        Hook for performing per time-slice tasks.
+
+        Parameters:
+            s (int): The sample number.
+            k (int): The time-slice number.
+        """
         pass
 
     def train(self, aux, w_latent, spike_latent):
-        pre_w = self.tau_pos*torch.exp(-aux/self.t_constant)*torch.gt(aux,0).int()
-        pos_w = self.tau_neg*torch.exp(-aux/self.t_constant)*torch.gt(aux,0).int()
+        """
+        Learning rule as per the original NeuCube-Py implementation.
+
+        Parameters:
+            aux (torch.Tensor): Element-wise time since last spike.
+            w_latent (torch.Tensor): Output weights.
+            spike_latent (torch.Tensor): Output spikes.
+        Returns:
+            pre_updates (torch.Tensor): Pre-synaptic updates.
+            pos_updates (torch.Tensor): Post-synaptic updates.
+        """
+        pre_w = self.a_pos*torch.exp(-aux/self.t_constant)*torch.gt(aux,0).int()
+        pos_w = self.a_neg*torch.exp(-aux/self.t_constant)*torch.gt(aux,0).int()
         pre_updates = pre_w*torch.gt((w_latent.T*spike_latent).T, 0).int()
         pos_updates = pos_w*torch.gt(w_latent*spike_latent, 0).int()
         return pre_updates, pos_updates
 
     def reset(self):
+        """
+        Hook for performing reset tasks.
+        """
         pass
